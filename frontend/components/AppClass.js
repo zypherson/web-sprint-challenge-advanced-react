31218860271faf6bff87d5cc06206f35ca6e34f1
zypherson <taylorzypherson@gmail.com>
2022-09-23 16:21:55 -0400

    updated sprint

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
 // Suggested initial states
-const initialMessage = ''
+/*const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
-const initialState = {
+  const initialState = {
   message: initialMessage,
   email: initialEmail,
   index: initialIndex,
   steps: initialSteps,
-}
+}*/
 
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  state = {
+    
+    totalMoves:0,
+    coordinateX: 2,
+    coordinateY: 2,
+    initialIndex: 4
+
+  }
 
   getXY = () => {
     // It it not necessary to have a state to track the coordinates.
@@ -30,18 +38,47 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState({...this.state, totalMoves: 0, coordinateX: 2, coordinateY:2, initialIndex: 4, totalMoves: 0})
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-  }
 
-  move = (evt) => {
+  }
+  
+
+  moveX = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    const newX = this.state.coordinateX + 1
+    const newIdx = this.state.initialIndex + 1
+    const newMovesx = this.state.totalMoves + 1
+    this.setState({...this.state, coordinateX: newX, initialIndex: newIdx, totalMoves: newMovesx})
   }
+
+  moveXleft = (evt) => {
+    const newX = this.state.coordinateX - 1
+    const newIdx = this.state.initialIndex - 1
+    const newMovesx = this.state.totalMoves + 1
+    this.setState({...this.state, coordinateX: newX, initialIndex: newIdx, totalMoves: newMovesx })
+  }
+
+  moveY = (evt) => {
+    const newY = this.state.coordinateY + 1
+    const newIdx = this.state.initialIndex - 3
+    const newMovesY = this.state.totalMoves + 1
+    this.setState({...this.state, coordinateY: newY, initialIndex: newIdx, totalMoves: newMovesY})
+  }
+
+  moveYdown = (evt) => {
+    const newY = this.state.coordinateY - 1
+    const newIdx = this.state.initialIndex + 3
+    const newMovesY = this.state.totalMoves + 1
+    this.setState({...this.state, coordinateY: newY, initialIndex: newIdx, totalMoves: newMovesY})
+  }
+  
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
@@ -56,14 +93,14 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates ${this.state.coordinateX}, ${this.state.coordinateY}`}</h3>
+          <h3 id="steps">{`You moved ${this.state.totalMoves} times`}</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.initialIndex ? ' active' : ''}`}>
+                {idx === this.state.initialIndex ? 'B' : null}
               </div>
             ))
           }
@@ -72,11 +109,11 @@ export default class AppClass extends React.Component {
           <h3 id="message"></h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.moveXleft}>LEFT</button>
+          <button id="up" onClick={this.moveY}>UP</button>
+          <button id="right" onClick={this.moveX}>RIGHT</button>
+          <button id="down"onClick={this.moveYdown} >DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
         <form>
           <input id="email" type="email" placeholder="type email"></input>
