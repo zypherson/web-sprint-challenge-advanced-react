2c010197893d01f463c8b2c0fab4d0c4a85a3d52
zypherson <taylorzypherson@gmail.com>
2022-09-29 12:30:12 -0400

    updated sprint

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 /*const initialMessage = ''
@@ -12,16 +13,22 @@ const initialIndex = 4 // the index the "B" is at
   index: initialIndex,
   steps: initialSteps,
 }*/
+const url = 'http://localhost:9000/api/result'
+const initialEmail =  ''
 
 export default class AppClass extends React.Component {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+ 
+  
   state = {
     
     totalMoves:0,
     coordinateX: 2,
     coordinateY: 2,
-    initialIndex: 4
+    initialIndex: 4,
+    message:'',
+    email: initialEmail
 
   }
 
@@ -38,7 +45,7 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
-    this.setState({...this.state, totalMoves: 0, coordinateX: 2, coordinateY:2, initialIndex: 4, totalMoves: 0})
+    this.setState({...this.state, totalMoves: 0, coordinateX: 2, coordinateY:2, initialIndex: 4, totalMoves: 0, email: initialEmail})
   }
 
   getNextIndex = (direction) => {
@@ -52,6 +59,11 @@ export default class AppClass extends React.Component {
   moveX = (evt) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    if(this.state.coordinateX > 2 ) {
+      return this.setState({...this.state, message: "You can't go right"})
+    }
+    
+    
     const newX = this.state.coordinateX + 1
     const newIdx = this.state.initialIndex + 1
     const newMovesx = this.state.totalMoves + 1
@@ -59,6 +71,10 @@ export default class AppClass extends React.Component {
   }
 
   moveXleft = (evt) => {
+    if(this.state.coordinateX < 2 ) {
+      return this.setState({...this.state, message: "You can't go left"})
+    }
+    
     const newX = this.state.coordinateX - 1
     const newIdx = this.state.initialIndex - 1
     const newMovesx = this.state.totalMoves + 1
@@ -66,34 +82,62 @@ export default class AppClass extends React.Component {
   }
 
   moveY = (evt) => {
-    const newY = this.state.coordinateY + 1
+    if(this.state.coordinateY < 2 ) {
+      return this.setState({...this.state, message: "You can't go up"})
+    }
+    const newY = this.state.coordinateY - 1
     const newIdx = this.state.initialIndex - 3
     const newMovesY = this.state.totalMoves + 1
     this.setState({...this.state, coordinateY: newY, initialIndex: newIdx, totalMoves: newMovesY})
   }
 
   moveYdown = (evt) => {
-    const newY = this.state.coordinateY - 1
+    if(this.state.coordinateY > 2 ) {
+      return this.setState({...this.state, message: "You can't go down"})
+    }
+    const newY = this.state.coordinateY + 1
     const newIdx = this.state.initialIndex + 3
     const newMovesY = this.state.totalMoves + 1
-    this.setState({...this.state, coordinateY: newY, initialIndex: newIdx, totalMoves: newMovesY})
+    this.setState({...this.state, coordinateY: newY, initialIndex: newIdx, totalMoves: newMovesY })
   }
   
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
+    const {value} = evt.target
+    this.setState({...this.state, email: value})
+    console.log(value)
   }
 
   onSubmit = (evt) => {
+    console.log('submitting')
+    evt.preventDefault()
+    let message 
     // Use a POST request to send a payload to the server.
+    axios.post(url, { "x": this.state.coordinateX, "y": this.state.coordinateY, "steps": this.state.totalMoves, "email": this.state.email })
+      .then(res => {
+        message = res.data.message
+      })
+      .catch(err => {
+        message =  err.response.data.message
+      })
+
+      .finally(() => {
+          this.reset();
+          this.setState({...this.state, message, email:initialEmail})
+      })
+
+      
   }
+  
+ 
 
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">{`Coordinates ${this.state.coordinateX}, ${this.state.coordinateY}`}</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.coordinateX}, ${this.state.coordinateY})`}</h3>
           <h3 id="steps">{`You moved ${this.state.totalMoves} times`}</h3>
         </div>
         <div id="grid">
@@ -106,7 +150,7 @@ export default class AppClass extends React.Component {
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button id="left" onClick={this.moveXleft}>LEFT</button>
@@ -115,9 +159,9 @@ export default class AppClass extends React.Component {
           <button id="down"onClick={this.moveYdown} >DOWN</button>
           <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
-          <input id="submit" type="submit"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" value={this.state.email} onChange={this.onChange} type="email" placeholder="type email"></input>
+          <input id="submit" type="submit" ></input>
         </form>
       </div>
     )
