a429a9b3c35d4d2c0b0843772f39c8307ca38492
zypherson <taylorzypherson@gmail.com>
2022-10-04 14:13:12 -0400

    updated sprint

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,30 @@
+import AppFunctional from "./AppFunctional"
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+
+test('renders without errors', () => {
+  render(<AppFunctional/>)
+})
+
+test('when our app mounts, you moved 0 times appears to the screen', () => {
+  render(<AppFunctional/>)
+
+  const header = screen.queryByText(/You moved 0 times/i)
+
+  expect(header).toBeInTheDocument()
+})
+
+test('when our app mounts, "coordinates" appears to the screen', () => {
+  render(<AppFunctional/>)
+
+  const header = screen.getByText(/coordinates/i)
+
+  expect(header).toBeInTheDocument()
+})
+
